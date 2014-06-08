26f4ab34e3d502b814ed40ef7fd2ff658f21911c
Jeff Hann <jeffhann@gmail.com>
2014-06-08 12:56:23 -0300

    adding left/right arrows and ability to move between images

--- jquery.easygallery.js
+++ jquery.easygallery.js
@@ -1,7 +1,8 @@
 (function ($) {
-    $.fn.EasyGallery = function() {
-        var images = this.find("a");
+    var images, currentImage;
 
+    $.fn.EasyGallery = function() {
+        images = this.find("a");
         images.click(imageClick);
     };
 
@@ -12,15 +13,27 @@
     }
 
     drawWrapper = function(target) {
+        currentImage = target;
         var wrapper = $("<div></div>").addClass("eg-wrapper");
 
         var img = $("<img />").attr('src', target.attr("href")).load(function() {
             if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
-            alert('broken image!');
+                alert('broken image!');
             } else {
-            $(".eg-wrapper").append(img);
+                $(".eg-wrapper").append(img);
             }
         });
+
+        if (images.length > 1) {
+            var backArrow = $("<div></div>").addClass("eg-back");
+            var forwardArrow = $("<div></div>").addClass("eg-forward");
+
+            wrapper.append(backArrow);
+            wrapper.append(forwardArrow);
+
+            backArrow.click(previousImage);
+            forwardArrow.click(nextImage);
+        }
 
         var closeBtn = $("<div></div>").addClass("eg-close");
         wrapper.append(closeBtn);
@@ -33,6 +46,49 @@
         closeBtn.click(function(e) {
             $(".eg-wrapper").remove(); 
             $(".eg-overlay").remove();
+        });
+    }
+
+    nextImage = function(e) {
+        index = images.index(currentImage);
+        index++;
+
+        if (index >= images.length) {
+            index = 0;
+        }
+
+        currentImage = $(images[index]);
+
+        var img = $("<img />").attr('src', currentImage.attr("href")).load(function() {
+            if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+                alert('broken image!');
+            } else {
+                $(".eg-wrapper img").remove();
+                $(".eg-wrapper").append(img);
+            }
+        });
+    }
+
+    previousImage = function(e) {
+        index = images.index(currentImage);
+        index--;
+
+        if (index < 0) {
+            index = images.length - 1;
+        }
+
+        console.log(images.length);
+
+        currentImage = $(images[index]);
+        console.log(currentImage);
+
+        var img = $("<img />").attr('src', currentImage.attr("href")).load(function() {
+            if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+                alert('broken image!');
+            } else {
+                $(".eg-wrapper img").remove();
+                $(".eg-wrapper").append(img);
+            }
         });
     }
 
