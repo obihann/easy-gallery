e4cfae4c475ef114455d61e9b957194a5618269a
Jeff Hann <jeffhann@gmail.com>
2014-07-17 13:46:25 -0300

    adding max height and width to image wrapper so they don't go huge on mobile
    updating grunt script so it can minify css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,46 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      dist: {
-        files: {
-          'jquery.easygallery.min.js': ['jquery.easygallery.js'],
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            combine: {
+                files: {
+                    'easygallery.min.css': ['easygallery.css'],
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'jquery.easygallery.min.js': ['jquery.easygallery.js'],
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'jquery.easygallery.js', 'test/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        watch: {
+            files: ['jquery.easygallery.js'],
+            tasks: ['jshint', 'uglify', 'cssmin']
         }
-      }
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'jquery.easygallery.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['jquery.easygallery.js'],
-      tasks: ['jshint', 'uglify']
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('test', ['jshint']);
 
-  grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
 
 };
